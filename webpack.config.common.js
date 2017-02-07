ffd1a75f09f46b1b372e07880b2fee7b854adccc
Yoseph Anderson <yrganderson@gmail.com>
2017-02-06 17:35:07 -0700

    initial commit * dependencies * applications displays start text * commonjs * webpack development and production * basic styling * ts config

---
+++ webpack.config.common.js
@@ -0,0 +1,39 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        'app': './assets/app/main.ts'
+    },
+
+    resolve: {
+        extensions: ['.js', '.ts']
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                loaders: [
+                    'awesome-typescript-loader',
+                    'angular2-template-loader',
+                    'angular2-router-loader'
+                ]
+            },
+            {
+                test: /\.html$/,
+                loader: 'html'
+            },
+            {
+                test: /\.css$/,
+                loader: 'raw'
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.ContextReplacementPlugin(
+            /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
+            './src'
+        )
+    ]
+};
